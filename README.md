0f5e347fb7f22e092daf896de33e001887cb01d1
Navjot Dhanawat <n.dhanawat@splendornet.com>
2017-08-02 14:49:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to Setup: Dynamic handlebars html pdf to create dynamic content html to pdf.
+
+```
+npm install dynamic-html-pdf --save
+
+```
---
+++ test/index.js
@@ -0,0 +1,9 @@
+// var should = require('chai').should(),
+//     mailer = require('../index'),
+//     sendEmail = mailer.sendEmail;
+
+// describe('#sendEmail', function() {
+//   it('Sending Email', function() {
+//     sendEmail();
+//   });
+// });
